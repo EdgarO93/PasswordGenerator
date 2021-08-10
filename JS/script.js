672099936d43175a09775ab1fa4a20d1a7a43698
Edgar Ortega <edort93@gmail.com>
2021-08-10 08:44:02 -0500

    Created password criteria arrays

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,21 @@
+//creating variables, then splitting into arrays
+
+var abcLower = "abcdefghijklmnopqrstuvwxyz";
+var num = "0123456789";
+// have to add forward slashes to include qoutes and double forward slash to include on
+var symbol = "!#$%&'()*\"\+,-./:;<=>?@[\\]^_`{|}~";
+
+var abcArray = abcLower.split("");
+var numArray= num.split("");
+var symbolArray= symbol.split("");
+
+// make Array for upper ABCs
+var toCap =  function (x) {
+  return x.toUpperCase();
+};
+//The array(uppercase) is created by applying the above function (to make it uppercase) to the previous array (abclower)
+var abcUpArray = abcArray.map(toCap);
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
